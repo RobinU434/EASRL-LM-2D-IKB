03cb045ec0cac7f453419a0ff555dc3747364101
RobinU434 <robin.uhrich@arcor.de>
2022-12-17 13:54:40 +0100

    bonus reward if reached goal position with tolerance = epsilon

--- envs/task/reach_goal.py
+++ envs/task/reach_goal.py
@@ -3,15 +3,25 @@ from envs.task.base_task import BaseTask
 
 
 class ReachGoalTask(BaseTask):
-    def __init__(self, epsilon) -> None:
-        super().__init__(epsilon)
+    def __init__(self, epsilon: float = 0.01, n_time_steps: int = 200) -> None:
+        super().__init__(epsilon, n_time_steps)
 
     def reward(self, arm_postion, goal_postion):
-        reach_bonus = 10 if self.done(arm_postion, goal_postion) else 0
+        super().update()
+        reach_bonus = 10 if self.is_near_target(arm_postion, goal_postion) else 0
         
         return np.linalg.norm(arm_postion - goal_postion) + reach_bonus
 
     def done(self, arm_postion, goal_postion):
+        time_limit_reached = super().done()
+        is_near_target = self.is_near_target(arm_postion, goal_postion)
+
+        if is_near_target or time_limit_reached:
+            return True
+        else:
+            return False
+
+    def is_near_target(self, arm_postion, goal_postion):
         if np.linalg.norm(arm_postion - goal_postion) <= self._epsilon:
             return True
         else:

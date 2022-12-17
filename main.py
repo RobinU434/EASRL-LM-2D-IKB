d4b15d7b3b879dc246e284cd1cb61ab7d02f652b
RobinU434 <robin.uhrich@arcor.de>
2022-12-17 13:53:07 +0100

    implemented main file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
+import gym
 from algorithms.sac import SAC
 from envs.plane_robot_env import PlaneRobotEnv
+from envs.task.reach_goal import ReachGoalTask
 
 
-env = PlaneRobotEnv(4, 1)
+task = ReachGoalTask(epsilon=1)
+env = PlaneRobotEnv(
+  n_joints=500,
+  segment_lenght=1,
+  task=task)
+# env = gym.make('Pendulum-v1')
+
 sac = SAC(env)
 
-sac.train(2)
+sac.train(1000)

dbca4c7e5f09c51d674bf8467e0e3aa9c0c991d5
RobinU434 <robin.uhrich@arcor.de>
2022-12-18 09:20:14 +0100

    implemented test_pipeline

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 
 import gym
+import torch
+
+from torch.utils.tensorboard import SummaryWriter
+
 from algorithms.sac import SAC
 from envs.plane_robot_env import PlaneRobotEnv
 from envs.task.reach_goal import ReachGoalTask
 
 
-task = ReachGoalTask(epsilon=1)
-env = PlaneRobotEnv(
-  n_joints=500,
-  segment_lenght=1,
-  task=task)
-# env = gym.make('Pendulum-v1')
+num_joints = 10
 
-sac = SAC(env)
+for i in range(10):
+  seed = torch.seed()
+  task = ReachGoalTask(epsilon=0.1)
 
-sac.train(1000)
+  env = PlaneRobotEnv(
+    n_joints=num_joints,
+    segment_lenght=1,
+    task=task)
+
+  logger = SummaryWriter(f"results/{num_joints}_{seed}")
+
+  sac = SAC(
+    env,
+    logging_writer=logger
+    )
+
+  sac.train(1000)

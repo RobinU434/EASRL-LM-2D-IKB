943ef96cce7e4530decddddf1c2bc3b3953ce718
RobinU434 <robin.uhrich@arcor.de>
2022-12-30 15:53:54 +0100

    fixed bug with single action dimension for mean in Normal distr

--- algorithms/sac/policy_net.py
+++ algorithms/sac/policy_net.py
@@ -9,7 +9,7 @@ import numpy as np
 from torch.distributions import Normal
 from torch.distributions import constraints
 
-
+from algorithms.common.distributions import get_distribution
 
 class PolicyNet(nn.Module):
     def __init__(
@@ -18,12 +18,14 @@ class PolicyNet(nn.Module):
         input_size,
         output_size,
         init_alpha,
-        lr_alpha
+        lr_alpha,
+        action_sampling_mode: str = "independent",
+        covariance_decay: float = 0.5
         ):
         super(PolicyNet, self).__init__()
         self.fc1 = nn.Linear(input_size, 128)
         
-        self.fc_mu = nn.Linear(128, 1)
+        self.fc_mu = nn.Linear(128, output_size)
         self.fc_std  = nn.Linear(128, output_size)
 
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
@@ -32,6 +34,10 @@ class PolicyNet(nn.Module):
         self.log_alpha.requires_grad = True
         self.log_alpha_optimizer = optim.Adam([self.log_alpha], lr=lr_alpha)
 
+        self.covariance_decay = covariance_decay
+        self.action_sampling_mode = action_sampling_mode
+        self.action_sampling_func = get_distribution
+
     def forward(self, x):
         # squeeze input tensor from (x, 1) to (x, )
         x = np.squeeze(x)
@@ -39,8 +45,11 @@ class PolicyNet(nn.Module):
         mu = self.fc_mu(x)
         std = F.softplus(self.fc_std(x))
         
-        dist = Normal(mu, std, validate_args={"scale": constraints.greater_than_eq})
+        # dist = Normal(mu, std, validate_args={"scale": constraints.greater_than_eq})
+        dist = self.action_sampling_func(mu, std, self.action_sampling_mode, self.covariance_decay)
         action = dist.rsample()
+        # shape action for buffer layout
+        action = action.squeeze()
         log_prob = dist.log_prob(action)
         # sum log prob
         log_prob =  log_prob.sum() # independence assumption between individual propabbilities
@@ -48,7 +57,7 @@ class PolicyNet(nn.Module):
 
         real_action = torch.tanh(action)
         # Squash correction (from original SAC implementation)
-        # this comes from the fact that tanh is bijective and differentiable
+        # this comes from the fact that tanh is a bijection and differentiable
         real_log_prob = log_prob - torch.sum(torch.log(1 - torch.tanh(action).pow(2) + 1e-7))
 
         return real_action, real_log_prob

d8799614569374a8eb4791451bbd3962000b69c1
RobinU434 <robin.uhrich@arcor.de>
2022-12-30 15:52:39 +0100

    fixed bug for sparse mode

--- algorithms/common/distributions.py
+++ algorithms/common/distributions.py
@@ -1,17 +1,23 @@
 import math
 import torch
+
+from typing import Tuple
+
 from torch.distributions import MultivariateNormal
 
+from algorithms.helper.helper import get_dim
+from algorithms.common.helper import scale_matrix
 
-def get_distribution(loc: torch.tensor, mode: str = "independent", decay: float = 0) -> MultivariateNormal:
+
+def get_distribution(loc: torch.tensor, std: torch.tensor,  mode: str = "independent", decay: float = 0) -> MultivariateNormal:
     """main function. returns a Multivariate Normal distribution with covariance matrix either be: \n
     - independent (identity matrix) \n
-    - sparse (identity matrix plus neighbour diagonals have value == decay) \n
+    - sparse (identity matrix plus neighbor diagonals have value == decay) \n
     - exp_decay 
 
     Args:
         loc (torch.tensor): loc for distribution
-        mode (str, optional): mode for cavariance matrix. Defaults to "independent".
+        mode (str, optional): mode for covariance matrix. Defaults to "independent".
         decay (float, optional): decay for covariance matrix. Defaults to 0.
 
     Returns:
@@ -22,49 +28,68 @@ def get_distribution(loc: torch.tensor, mode: str = "independent", decay: float 
         sparse.__name__: sparse,
         exp_decay.__name__: exp_decay,
     }       
+    batch_size, n = get_dim(loc.size())
+    covariance_matrix = func_dict[mode](n, decay)
+    covariance_matrix = scale_matrix(covariance_matrix, batch_size)
 
-    convariance_matrix = func_dict[mode](len(loc), decay)
+    # get unit matrix
+    unit_matrix = independent(n, 0)
+    unit_matrix = scale_matrix(unit_matrix, batch_size)
+    unit_matrix *= std.unsqueeze(1)
+    covariance_matrix *=  unit_matrix
 
-    distribution = MultivariateNormal(loc, convariance_matrix)
+    distribution = MultivariateNormal(loc, covariance_matrix)
 
     return distribution
 
 
-def independent(n, decay):
-    # decay has to be there to fit the calling pattern
-    convariance_matrix = torch.eye(n)
-    return convariance_matrix
+def independent(n: torch.Size, decay: float) -> torch.tensor:
+    """returns unit matrix with shape (batch_size, m, m)
 
-def sparse(n, decay):
-    convariance_matrix = torch.eye(n)
+    Args:
+        size (int): the dimension of the result matrix
+        decay (float): place holder... not relevant in this function
+
+    Returns:
+        torch.tensor: independent covariance matrix
+    """
+    # decay has to be there to fit the calling pattern
+    covariance_matrix = torch.eye(n)
+    return covariance_matrix
+
+def sparse(n: int, decay: float) -> torch.tensor:
+
+    # TODO: do same thing as in function independent
+    covariance_matrix = torch.eye(n)
 
     # create matrix with decay one diagonal above the normal diagonal
     zeros = torch.zeros(1, n)
-    above = torch.cat((convariance_matrix[1:], zeros)) * decay
+    above = torch.cat((covariance_matrix[1:], zeros)) * decay
     # create matrix with decay one diagonal below the normal diagonal
-    below = torch.cat((zeros, convariance_matrix[:-1])) * decay
+    below = torch.cat((zeros, covariance_matrix[:-1])) * decay
 
-    # merge everything into the cavariance matrix
-    convariance_matrix = convariance_matrix + above + below
+    # merge everything into the covariance matrix
+    covariance_matrix = covariance_matrix + above + below
 
-    return convariance_matrix
+    return covariance_matrix
 
-def exp_decay(n, decay):
-    convariance_matrix = torch.eye(n)
-    sum_up_list = [convariance_matrix]
+def exp_decay(n: int, decay: float) -> torch.tensor:
+    # TODO: do same thing as in function independent
+    covariance_matrix = torch.eye(n)
+    sum_up_list = [covariance_matrix]
     for i in range(1, n):
         value = math.pow(decay, i)
         
         zeros = torch.zeros(i, n)
-        above = torch.cat((convariance_matrix[i:], zeros)) * value
-        below = torch.cat((zeros, convariance_matrix[:-i])) * value
+        above = torch.cat((covariance_matrix[i:], zeros)) * value
+        below = torch.cat((zeros, covariance_matrix[:-i])) * value
 
         sum_up_list.append(above)
         sum_up_list.append(below)
 
-    convariance_matrix = torch.stack(sum_up_list, dim=0).sum(dim=0)
+    covariance_matrix = torch.stack(sum_up_list, dim=0).sum(dim=0)
 
-    return convariance_matrix
+    return covariance_matrix
 
 if __name__ == "__main__":
     get_distribution(torch.tensor([1, 2, 3, 4]), mode="decay", decay=0.5)

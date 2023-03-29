a74db482a4ca215feb148393dbfc695c0882799c
RobinU434 <robin.uhrich@arcor.de>
2023-03-29 15:43:30 +0200

    new fields and logging methods

--- vae/model/vae.py
+++ vae/model/vae.py
@@ -1,25 +1,95 @@
+# https://avandekleut.github.io/vae/
+
 import torch
 import torch.nn as nn
+
+from torch.autograd import Variable
+from torch.utils.tensorboard import SummaryWriter
 
 from vae.model.decoder import Decoder
 from vae.model.encoder import Encoder
 
 
 class VariationalAutoencoder(nn.Module):
-    def __init__(self, input_dim, latent_dim, output_dim):
+    def __init__(self, input_dim, latent_dim, output_dim, learning_rate: float, logger: SummaryWriter, enhanced_latent_dim: int = 0):
         super(VariationalAutoencoder, self).__init__()
+        self.enhanced_latent_dim = enhanced_latent_dim
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.output_dim = output_dim
+
         self.encoder = Encoder(input_dim, latent_dim)
-        self.decoder = Decoder(latent_dim, output_dim)
+        self.decoder = Decoder(latent_dim + self.enhanced_latent_dim, output_dim)
 
         self.N = torch.distributions.Normal(0, 1)
         # self.N.loc = self.N.loc # .cuda() hack to get sampling on the GPU
         # self.N.scale = self.N.scale # .cuda()
 
-    def forward(self, x):
-        mu, log_std = self.encoder(x)
+        self.logger: SummaryWriter = logger
+
+        self.decoder_history = torch.tensor([])
+        self.z_grad_history = torch.tensor([])
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+
+    def forward(self, x, latent_enhancer: torch.tensor = torch.tensor([])):
+        mu, log_std = self.encoder(x)  # output dim (batch_size, latent_space)
         
         # sample the latent space
         sigma = torch.exp(log_std)
         z = mu + sigma * self.N.sample(mu.shape)
 
-        return self.decoder(z), mu, log_std
+        # enhance latent space
+        z = torch.cat([z, latent_enhancer], dim=1)
+        z = Variable(z, requires_grad=True)
+        # store for logging the gradient
+        self.z = z
+
+        decoder_out = self.decoder.forward(z) 
+        self.decoder_history = torch.cat([self.decoder_history, decoder_out.flatten()])
+
+        return decoder_out, mu, log_std
+
+    def train(self, loss):
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        # log gradient from latent space
+        z_grad = self.z.grad
+        self.z_grad_history = torch.cat([self.z_grad_history, z_grad], dim=0)
+
+        self.optimizer.step()
+
+    def log_parameters(self, epoch_idx):
+        # parameter histogram
+        param_tensor = torch.tensor([])
+        for param in self.parameters():
+            param_tensor = torch.cat([param_tensor, param.flatten()])
+        self.logger.add_histogram("vae/param", param_tensor, epoch_idx)
+
+    def log_gradients(self, epoch_idx):
+        grad_tensor = torch.tensor([])
+        for param in self.parameters():
+            grad_tensor = torch.cat([grad_tensor, param.grad.flatten()])
+        self.logger.add_histogram("vae/grad", grad_tensor, epoch_idx)
+
+    def log_decoder_distr(self, epoch_idx):
+        self.logger.add_histogram("vae/decoder_distr", self.decoder_history, epoch_idx)
+
+    def log_z_grad(self, epoch_idx):
+        # taking the absolute
+        z_grad_abs = torch.abs(self.z_grad_history)
+        z_grad_abs = z_grad_abs.sum(dim=0)
+        # normalize inputs
+        z_grad_abs /= torch.norm(z_grad_abs)
+        z_grad_abs = z_grad_abs.unsqueeze(dim=0)
+        z_grad_abs = z_grad_abs.unsqueeze(dim=0)
+
+        self.logger.add_image("vae/z_grad", z_grad_abs, epoch_idx)
+
+    def reset_history(self):
+        """
+        call this function before an epoch to ensure that there is only data from one epoch inside 
+        """
+        self.decoder_history = torch.tensor([])
+        self.z_grad_history = torch.tensor([])

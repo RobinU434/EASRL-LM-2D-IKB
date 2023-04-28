395a3ba80b2d401b00811b57c332425a10941d11
RobinU434 <robin.uhrich@arcor.de>
2023-04-28 21:19:35 +0200

    added portforwarding script

---
+++ port_forwarding.sh
@@ -0,0 +1,3 @@
+ssh -N -f -L localhost:6006:localhost:6006 uhrich@nrgpu3.imbit.privat
+ssh -N -f -L localhost:6007:localhost:6007 uhrich@nrgpu3.imbit.privat
+ssh -N -f -L localhost:6008:localhost:6008 uhrich@nrgpu3.imbit.privat

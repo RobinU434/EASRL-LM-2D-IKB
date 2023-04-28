dcb1b1f95ce6d4a316511ff6ba41cd024e60e5d8
RobinU434 <robin.uhrich@arcor.de>
2023-04-28 21:17:15 +0200

    finished ddpg

--- algorithms/ddpg/ddpg.py
+++ algorithms/ddpg/ddpg.py
@@ -65,16 +65,19 @@ class DDPG:
         return target
 
     def train_q(self, s, a, target):
-        q_loss = F.smooth_l1_loss(self._q(s, a), target.detach())
+        zeros = torch.zeros_like(self._q(s, a), requires_grad=True)
+        q_loss = F.smooth_l1_loss(zeros, target.detach())
         self._q.train(q_loss)
+        return q_loss
     
     def train_mu(self, s):
         mu_loss = -self._q(s, self._mu(s)).mean() # That's all for the policy loss.
         self._mu.train(mu_loss)
+        return mu_loss
 
     def train(self, n_epochs: int, print_interval: int = 20):
         score = 0.0
-       
+        num_steps = 0
         for epoch_idx in range(n_epochs):
             s = self._env.reset()
             done = False
@@ -86,21 +89,56 @@ class DDPG:
                 self._memory.put((s, a, r, s_prime, done))
                 score = score + r
                 s = s_prime
-                    
+
+            num_steps = num_steps + self._env.num_steps
+            
+            q_loss_list = []
+            mu_loss_list = []
             if len(self._memory) > self._start_buffer_size:
                 for i in range(self._train_iterations):
                     s, a, r, s_prime, done_mask  = self._memory.sample(self._batch_size)
                     target = self.calc_target(r, s_prime, done_mask)
-                    self.train_q(s, a, target)
-                    self.train_mu(s)
+                    q_loss = self.train_q(s, a, target)
+                    q_loss_list.append(q_loss.detach())
+                    mu_loss = self.train_mu(s)
+                    mu_loss_list.append(mu_loss.detach())
 
                     # train(self._mu, mu_target, q, q_target, memory, q_optimizer, mu_optimizer)
                     soft_update(self._mu, self._mu_target, self._tau)
                     soft_update(self._q,  self._q_target, self._tau)
             
             if epoch_idx % print_interval == 0 and epoch_idx != 0:
-                print("# of episode :{}, avg score : {:.1f}".format(epoch_idx, score/print_interval))
+                avg_episode_len = num_steps / print_interval
+                mean_reward = score / num_steps
+                print("# of episode :{}, avg score : {:.1f}".format(epoch_idx, mean_reward))
+                
+                if self._logger is not None:
+                    self._logger.add_scalar("stats/mean_reward", mean_reward, epoch_idx)
+                    self._logger.add_scalar("stats/mean_episode_len", avg_episode_len, epoch_idx)
+                    self._logger.add_scalar("ddpg/mu_loss", torch.tensor(mu_loss_list).mean(), epoch_idx)
+                    self._logger.add_scalar("ddpg/q_loss", torch.tensor(q_loss_list).mean(), epoch_idx)
+
+                if self._fs_logger is not None:
+                    self._fs_logger.add_scalar("stats/mean_reward", mean_reward, epoch_idx)
+                    self._fs_logger.add_scalar("stats/mean_episode_len", avg_episode_len, epoch_idx)
+                    self._fs_logger.add_scalar("ddpg/mu_loss", torch.tensor(mu_loss_list).mean(), epoch_idx)
+                    self._fs_logger.add_scalar("ddpg/q_loss", torch.tensor(q_loss_list).mean(), epoch_idx)
+                
+                torch.save({
+                    "epoch": epoch_idx,
+                    "mu_model_state": self._mu.state_dict(),
+                    "mu_optimizer_state_dict": self._mu.optimizer.state_dict(),
+                    "mu_target_model_state": self._mu_target.state_dict(),
+                    "mu_target_optimizer_state_dict": self._mu_target.optimizer.state_dict(),
+                    "q_state_dict": self._q.state_dict(),
+                    "q_optimizer_state_dict": self._q.optimizer.state_dict(),
+                    "q_target_state_dict": self._q_target.state_dict(),
+                    "q_target_optimizer_state_dict": self._q_target.optimizer.state_dict(),
+                    "reward": float(mean_reward),
+                }, self._fs_logger.path + f"model_{epoch_idx}_reward_{mean_reward:.4f}.pt")
+
                 score = 0.0
+                num_steps = 0
 
         self.env.close()
 
--- algorithms/ddpg/q_net.py
+++ algorithms/ddpg/q_net.py
@@ -12,22 +12,22 @@ class QNet(nn.Module):
         self._learning_rate = learning_rate
 
         self.fc_s = nn.Linear(state_dim, 64)
-        self.fc_a = nn.Linear(action_dim,64)
+        self.fc_a = nn.Linear(action_dim, 64)
         self.fc_q = nn.Linear(128, 32)
         self.fc_out = nn.Linear(32, output_dim)
-
+        
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
-    def forward(self, x, a):
+    def forward(self, x: torch.tensor, a: torch.tensor) -> torch.tensor:
         h1 = F.relu(self.fc_s(x))
         h2 = F.relu(self.fc_a(a))
         cat = torch.cat([h1, h2], dim=1)
         q = F.relu(self.fc_q(cat))
         q = self.fc_out(q)
+
         return q
     
-    def train(self, loss: torch.tensor):
-        print(loss)
+    def train(self, loss: torch.tensor) -> None:
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()

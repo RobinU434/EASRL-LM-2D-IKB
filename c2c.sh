76539b842703efea4d39c20c65d3a8d3c24a9977
Robin Uhrich <robin.uhrich@gmail.com>
2025-06-20 13:11:13 +0200

    added c2c routine

--- c2c.sh
+++ c2c.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-config2class stop-all
+c2c stop-all
 
-
-config2class to-code --input configs/training/train_sac.yaml --output project/utils/configs/train_sac.py
+c2c hydra2code --input configs/train_sac.yaml --output project/utils/configs/train_sac.py

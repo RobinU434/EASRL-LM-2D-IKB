762f2e91302ccbef329a540dc5c4b77a3567e7a5
Robin Uhrich <robin.uhrich@gmail.com>
2025-09-21 19:34:36 +0200

    added flow actor

--- project/models/actor.py
+++ project/models/actor.py
@@ -8,6 +8,8 @@ from stable_baselines3.common.torch_layers import create_mlp
 from stable_baselines3.common.type_aliases import PyTorchObs
 from stable_baselines3.sac.policies import Actor
 
+from project.models.flow import ConditionalRealNVP, RealNVP
+
 
 class LatentActor(Actor):
     def __init__(
@@ -70,7 +72,135 @@ class LatentActor(Actor):
                 activation_fn=activation_fn,
             )
         )
-        
+
+    def get_action_dist_params(self, obs):
+        mean_actions, log_std, kwargs = super().get_action_dist_params(obs)
+        if self.constrain_latent_space:
+            # squeeze mean into a constrained space [-1, 1]
+            mean_actions = torch.tanh(mean_actions)
+        return mean_actions, log_std, kwargs
+
+    def get_latent_action(
+        self, observation: torch.Tensor, deterministic: bool = False
+    ) -> torch.Tensor:
+        return super().forward(observation, deterministic)
+
+    def get_decoded_action(
+        self, z: torch.Tensor, observation: torch.Tensor
+    ) -> torch.Tensor:
+        """Get the decoded action from the latent space by passing through the decoder network.
+
+        Args:
+            z (torch.Tensor): The latent space representation.
+            observation (torch.Tensor): The original observation.
+
+        Returns:
+            torch.Tensor: The decoded action.
+        """
+        # Apply additional network to the sampled action
+        if self.conditional_decoder:
+            z = torch.cat([z, observation], dim=-1)
+
+        action = self.decoder.forward(z)
+        return torch.tanh(action)  # Tanh to squash action to [-1, 1]
+
+    def forward(self, obs: torch.Tensor, deterministic: bool = False) -> torch.Tensor:
+        """Get the action corresponding to the observation by sampling from the latent space and decoding it.
+
+        Args:
+            obs (torch.Tensor): The original obs.
+            deterministic (bool, optional): Whether to use deterministic action selection. Defaults to False.
+
+        Returns:
+            torch.Tensor: The latent action.
+        """
+        z = self.get_latent_action(obs, deterministic)
+        action = self.get_decoded_action(z, obs)
+        return action
+
+    def action_log_prob(self, obs: PyTorchObs) -> Tuple[torch.Tensor, torch.Tensor]:
+        latent_action, log_prob = super().action_log_prob(obs)
+        action = self.get_decoded_action(latent_action, obs)
+        return action, log_prob
+
+    def _predict(self, observation, deterministic=False):
+        return self.forward(observation, deterministic)
+
+
+class FlowActor(Actor):
+    def __init__(
+        self,
+        observation_space,
+        action_space,
+        net_arch,
+        features_extractor,
+        features_dim,
+        latent_dim: int,
+        flow_arch: List[int] = [256, 256],
+        conditional_flow: bool = False,
+        constrain_latent_space: bool = False,
+        activation_fn=nn.ReLU,
+        use_sde=False,
+        log_std_init=-3,
+        full_std=True,
+        use_expln=False,
+        clip_mean=2,
+        normalize_images=True,
+    ):
+        # adapt action space to reduce output dim of Gaussian distribution and input dim of decoder
+
+        assert isinstance(action_space, Box), "The action space has to be Box."
+        assert len(action_space.shape) == 1, "Assert vectorized action space."
+
+        latent_action_space = Box(
+            low=-1,  # regularize with tanh
+            high=1,
+            shape=(latent_dim,),
+            dtype=action_space.dtype,
+            seed=action_space._np_random,
+        )
+
+        super().__init__(
+            observation_space,
+            latent_action_space,
+            net_arch,
+            features_extractor,
+            features_dim,
+            activation_fn,
+            use_sde,
+            log_std_init,
+            full_std,
+            use_expln,
+            clip_mean,
+            normalize_images,
+        )
+
+        self.conditional_flow = conditional_flow
+        self.constrain_latent_space = constrain_latent_space
+        input_dim = latent_dim
+        if self.conditional_flow:
+            input_dim += self.features_dim
+            self.flow_model = nn.Sequential(
+                nn.Linear(latent_dim, get_action_dim(action_space)),
+                ConditionalRealNVP(
+                    dim=get_action_dim(action_space),
+                    cond_dim=features_dim,
+                    n_couplings=len(flow_arch),
+                    hidden_dim=flow_arch[0],
+                    device=self.device,
+                ),
+            )
+        else:
+            self.flow_model = nn.Sequential(
+                nn.Linear(latent_dim, get_action_dim(action_space)),
+                RealNVP(
+                    dim=get_action_dim(action_space),
+                    n_couplings=len(flow_arch),
+                    hidden_dim=flow_arch[0],
+                    device=self.device,
+                ),
+            )
+
     def get_action_dist_params(self, obs):
         mean_actions, log_std, kwargs = super().get_action_dist_params(obs)
         if self.constrain_latent_space:
@@ -78,29 +208,32 @@ class LatentActor(Actor):
             mean_actions = torch.tanh(mean_actions)
         return mean_actions, log_std, kwargs
     
-    def get_latent_action(self, observation: torch.Tensor, deterministic: bool = False
+    def get_latent_action(
+        self, observation: torch.Tensor, deterministic: bool = False
     ) -> torch.Tensor:
         return super().forward(observation, deterministic)
 
-    def get_decoded_action(self, z: torch.Tensor, observation: torch.Tensor) -> torch.Tensor:
-        # Apply additional network to the sampled action
-        if self.conditional_decoder:
-            z = torch.cat([z, observation], dim=-1)
-        
-        action = self.decoder.forward(z)
-        return torch.tanh(action)  # Tanh to squash action to [-1, 1]
 
-    def forward(
-        self, observation: torch.Tensor, deterministic: bool = False
-    ) -> torch.Tensor:
-        z = self.get_latent_action(observation, deterministic)
-        action = self.get_decoded_action(z, observation)
+    def get_flow_action(self, z: torch.Tensor, obs: torch.Tensor) -> torch.Tensor:
+        if self.conditional_flow:
+            z = torch.cat([z, obs], dim=-1)
+        action, log_det = self.flow_model.forward(z)
+        return torch.tanh(action), log_det
+
+    def forward(self, obs, deterministic=False):
+        z = self.get_latent_action(obs, deterministic)
+        action = self.get_flow_action(z, obs)
         return action
     
     def action_log_prob(self, obs: PyTorchObs) -> Tuple[torch.Tensor, torch.Tensor]:
         latent_action, log_prob = super().action_log_prob(obs)
-        action = self.get_decoded_action(latent_action, obs)
+        action, logdet = self.get_flow_action(latent_action, obs)
+        # p(x): distribution after flow
+        # p(z): distribution before flow
+        # 1 / |det dx/dz|: change of variables
+        # p(x) = p(z) * 1 / |det dx/dz|
+        log_prob = log_prob + logdet
         return action, log_prob
-    
+
     def _predict(self, observation, deterministic=False):
         return self.forward(observation, deterministic)
